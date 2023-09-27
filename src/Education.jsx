f9cf8770f072e539d8ebc23673136def5766ed1b
Philip Pretorius <philippretorius.79@gmail.com>
2023-09-27 16:29:11 +0200

    Exp and education components: objects, useState, onChange, and p with output added

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,20 +1,36 @@
+import { useState } from "react"
+
+const degree =
+  {
+    degree: 'Bcom (Industrial Psychology)',
+    institution: 'Stellenbosch University',
+    start: 2018,
+    end: 2020,
+    id: 0
+  }
 
 function Degree() {
+  const [degreeInfo, setDegreeInfo] = useState(degree)
+
   return (
     <>
       <label htmlFor="degree">Degree:{' '}
         <input 
           id="degree"
           type="text" 
+          onChange={(e) => setDegreeInfo({ ...degreeInfo, degree: e.target.value })}
         />
       </label>
+      <p>{degreeInfo.degree}</p>
       <br />
       <label htmlFor="institution">Institution:{' '}
         <input 
           id="institution"
           type="text" 
+          onChange={(e) => setDegreeInfo({ ...degreeInfo, institution: e.target.value })}
         />
       </label>
+      <p>{degreeInfo.institution}</p>
       <br />
       <label htmlFor="startDate">Start Date:{' '}
         <input 
@@ -22,8 +38,10 @@ function Degree() {
           type="number"
           min="1900" 
           max="2023"
+          onChange={(e) => setDegreeInfo({ ...degreeInfo, start: e.target.value })}
         />
       </label>
+      <p>{degreeInfo.start}</p>
       <br />
       <label htmlFor="endDate">End Date:{' '}
         <input 
@@ -31,8 +49,10 @@ function Degree() {
           type="number"
           min="1900" 
           max="2023"
+          onChange={(e) => setDegreeInfo({ ...degreeInfo, end: e.target.value })}
         />
       </label>    
+      <p>{degreeInfo.end}</p>
     </>
   )
 }
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,29 +1,37 @@
-/* const job = [
+import { useState } from "react"
+
+const job =
   {
-    title: 'Job Title',
-    company: 'Company',
-    start: 'Start',
-    end: 'End'
-  },
-] */
+    title: 'Software developer',
+    company: 'Bchekcd',
+    start: 2020,
+    end: 2023,
+    id: 0
+  }
 
 function Job() {
+  const [jobInfo, setJobInfo] = useState(job)
+
   return (
     <>
-      <h2>Education</h2>
+      <h2>Experience</h2>
       <label htmlFor="title">Job title:{' '}
         <input 
           id="title"
           type="text" 
+          onChange={(e) => setJobInfo({ ...jobInfo, title: e.target.value })}
         />
       </label>
+      <p>{jobInfo.title}</p>
       <br />
       <label htmlFor="institution">Company:{' '}
         <input 
           id="institution"
           type="text" 
+          onChange={(e) => setJobInfo({ ...jobInfo, company: e.target.value })}          
         />
       </label>
+      <p>{jobInfo.company}</p>
       <br />
       <label htmlFor="startDate">Start Date:{' '}
         <input 
@@ -31,8 +39,10 @@ function Job() {
           type="number"
           min="1900" 
           max="2023"
+          onChange={(e) => setJobInfo({ ...jobInfo, start: e.target.value })}
         />
       </label>
+      <p>{jobInfo.start}</p>
       <br />
       <label htmlFor="endDate">End Date:{' '}
         <input 
@@ -40,8 +50,10 @@ function Job() {
           type="number"
           min="1900" 
           max="2023"
+          onChange={(e) => setJobInfo({ ...jobInfo, end: e.target.value })}
         />
-      </label>    
+      </label>  
+      <p>{jobInfo.end}</p>  
     </>
   )
 }
