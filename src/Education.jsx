761b273a2d9c8b4581856ba426f54ef34c98118d
Philip Pretorius <philippretorius.79@gmail.com>
2023-09-29 23:56:21 +0200

    Buttons

--- src/Education.jsx
+++ src/Education.jsx
@@ -7,41 +7,48 @@ export default function Education({
 
   return (
     <div className="eduDiv">
-      <label htmlFor="degree">Degree:{' '}
-        <input
-          id="degree"
-          type="text"
-          onChange={changeDegree}
-        />
-      </label>
-      <br />
-      <label htmlFor="institution">Institution:{' '}
-        <input
-          id="institution"
-          type="text"
-          onChange={changeInstitution}
-        />
-      </label>
-      <br />
-      <label htmlFor="startDate">Start Date:{' '}
-        <input
-          id="startDate"
-          type="number"
-          min="1900"
-          max="2023"
-          onChange={changeStart}
-        />
-      </label>
-      <br />
-      <label htmlFor="endDate">End Date:{' '}
-        <input
-          id="endDate"
-          type="number"
-          min="1900"
-          max="2023"
-          onChange={changeEnd}
-        />
-      </label>
+      <div className="divDiv">
+        <label htmlFor="degree">Degree:{' '}
+          <input
+            id="degree"
+            type="text"
+            onChange={changeDegree}
+          />
+        </label>
+        <br />
+        <label htmlFor="institution">Institution:{' '}
+          <input
+            id="institution"
+            type="text"
+            onChange={changeInstitution}
+          />
+        </label>
+        <br />
+        <label htmlFor="startDate">Start Date:{' '}
+          <input
+            id="startDate"
+            type="number"
+            min="1900"
+            max="2023"
+            onChange={changeStart}
+          />
+        </label>
+        <br />
+        <label htmlFor="endDate">End Date:{' '}
+          <input
+            id="endDate"
+            type="number"
+            min="1900"
+            max="2023"
+            onChange={changeEnd}
+          />
+        </label>
+      </div>
+      <div>
+        <button>Add</button>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div> 
   )
 }
